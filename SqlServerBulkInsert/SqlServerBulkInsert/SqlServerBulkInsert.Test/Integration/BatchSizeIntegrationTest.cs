4c12c314a457e029aa06e87f3ef1755010600cee
Philipp Wagner <bytefish@gmx.de>
2016-10-15 11:23:49 +0200

    Removed Usings

--- SqlServerBulkInsert/SqlServerBulkInsert/SqlServerBulkInsert.Test/Integration/BatchSizeIntegrationTest.cs
+++ SqlServerBulkInsert/SqlServerBulkInsert/SqlServerBulkInsert.Test/Integration/BatchSizeIntegrationTest.cs
@@ -6,8 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SqlServerBulkInsert.Test.Integration
 {
